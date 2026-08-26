4533627052801fc3d84cf8ca3d764cd4414a8b72
MihirMaurya-dev <chhotuverma2238@gmail.com>
2026-08-26 16:47:47 +0530

    new resources

--- test/main.tf
+++ test/main.tf
@@ -1,23 +1,31 @@
-terraform{
-    required_providers{
-        azurerm={
-           source="hashicorps/azurerm"
-           version="5.0.0"
-        }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.0.0"
     }
+  }
 }
 
-provider "azurerm"{
-    features{}
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "newrg" {
-  name = rg1
-  location = eastus
+  name     = "rg1"
+  location = "eastus"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-    name = vnet1
-    laoction = eastus
-    resource_group_name = rg1
+  name                = "vnet1"
+  location            = azurerm_resource_group.newrg.location
+  resource_group_name = azurerm_resource_group.newrg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.newrg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
